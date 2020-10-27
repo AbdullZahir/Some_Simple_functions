4b857027fa4a273c4dd08c6af95ba2197987dfe8
AbdullahZahir <AbdullahZahir@github.com>
2020-10-27 16:40:46 +0100

    Adding additional information

--- RMSE_images.py
+++ RMSE_images.py
@@ -6,6 +6,8 @@ Created on Tue Oct 27 16:28:46 2020
 """
 import numpy as np
 
+#remember to convert images to arrays
+
 def RMSE_img(image_x,image_y):
     error=np.sqrt(np.mean(np.square(image_x-image_y)))
     return error
