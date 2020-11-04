5ccfb4285814e94f7d4f41399e587bbcf7a4a5b7
Abdullah Zahir Babar <62884467+AbdullZahir@users.noreply.github.com>
2020-11-04 16:15:58 +0100

    Update data_generator_machine_learning.py
    Adding comment to make it understandable

--- data_generator_machine_learning.py
+++ data_generator_machine_learning.py
@@ -8,7 +8,7 @@ cnt= 0
 
 for item1 in folder1: 
 	for item2 in folder2: 
-		if cnt<2001: 
+		if cnt<2001: #counting trough 2000 files/images.
 			if(item1.strip('.jpg')==item2.strip('.xml')):
 				shutil.copy('path to your imagefolder'+item1,'destination folder for image folder')
 				shutil.copy('path to your xml folder'+item2,'destination folder for xml files') 
